9f20f48a38a0d7344e4ff7193f6f6f560bfd959f
Gia Bao <baodngworkstation@gmail.com>
2026-01-29 00:05:40 +0700

    setup

---
+++ components/ScreenContent.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { EditScreenInfo } from './EditScreenInfo';
+
+type ScreenContentProps = {
+  title: string;
+  path: string;
+  children?: React.ReactNode;
+};
+
+export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
+  return (
+
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <EditScreenInfo path={path} />
+    </View>
+  );
+};
